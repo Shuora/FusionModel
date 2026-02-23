d4661ee458f73a532a8fd4b6aaaf1a8a4bfdf50b
zhuangshuo <zhuang.shuo@qq.com>
2026-02-23 13:13:26 +0800

    feat: clear

--- tests/config/test_config_loading.py
+++
@@ -1,6 +0,0 @@
-def test_dataset_profile_has_required_keys():
-    from src.common.config import load_yaml
-
-    cfg = load_yaml("configs/dataset_tls_full.yaml")
-    required = {"dataset_name", "source_root", "split", "tls_filter", "feature"}
-    assert required.issubset(set(cfg.keys()))
--- tests/pipeline/test_token_encoder.py
+++
@@ -1,6 +0,0 @@
-def test_token_encoder_has_cls_sep():
-    from src.pipeline.token_encoder import encode_tls_tokens
-
-    ids = encode_tls_tokens({"records": [], "handshake": {}}, max_len=32)
-    assert ids[0] == 101
-    assert ids[-1] == 102
